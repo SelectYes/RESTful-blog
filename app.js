8a7d15ce4a9948627665956fb1ddd683b0342c9f
Lenny <riel_lenny@yahoo.co.uk>
2020-07-28 18:25:48 -0500

    I did some things

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/restful_blog', {
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}))
 
 ////////////////////////////////////////////////////////////////////////////
 //                               MODEL CONFIG
@@ -31,18 +32,36 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-Blog.create({
-    title: "Hello World!",
-    image: "www.image.com",
-    body: "This is my first blog post!",
-});
-
 ////////////////////////////////////////////////////////////////////////////
 //                                 ROUTES
 ////////////////////////////////////////////////////////////////////////////
 
+app.get('/', (req, res) => {
+    res.redirect('/blogs');
+});
+
 app.get('/blogs', (req, res) => {
-    res.render('index');
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {blogs: blogs});
+        }
+    })
+});
+
+app.post('/blogs', (req, res) => {
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect('/blogs')
+        }
+    })   
+})
+
+app.get('/blogs/new', (req, res) => {
+    res.render('new')
 });
 
 app.listen(port, () => console.log('Serving RESTful Blog on localhost:' + port));
